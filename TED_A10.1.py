140f732c29af7837da5f2b943d4823fbfdfc8a31
vinicls <101654694+vinicls@users.noreply.github.com>
2022-05-30 15:29:57 -0300

    TED 10

---
+++ TED_A10.1.py
@@ -0,0 +1,8 @@
+#TED AULA 10
+#QUESTÃO 1
+
+numeros = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"]
+print(numeros)
+
+numeros.reverse()
+print(numeros)
