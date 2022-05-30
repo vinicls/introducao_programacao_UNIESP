cdb1c1bab447250b95dd1a87dde27b798bda5378
vinicls <101654694+vinicls@users.noreply.github.com>
2022-05-30 15:20:57 -0300

    Add files via upload

---
+++ TED_A11.py
@@ -0,0 +1,20 @@
+#TED AULA 11
+
+linha,coluna = (10,10)
+from random import randint
+
+matriz1 = []
+
+for l in range(linha):
+    lin = []
+    for c in range(coluna):
+        lin.append(randint(1,100))
+    matriz1.append(lin)
+
+print(matriz1)
+
+soma = 0
+for l in range(linha):
+    for c in range(coluna):
+        soma +=matriz1[l][c]
+print("A soma de todos os vetores da matriz é {}".format(soma)) 
